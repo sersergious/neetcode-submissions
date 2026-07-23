973d03616e20bdc390281a84a1acea72b076b47f
Serhii Kuzmin <sergeykuzmin495@gmail.com>
2026-07-23 18:10:44 -0400

    Add: eating-bananas - submission-13

---
+++ Data Structures & Algorithms/eating-bananas/submission-13.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Solution time: 43 minutes - 2nd time
+    # Reflection: got 70% of the algo by myself with hints
+    # the rest with the help of AI. I was stuck with the computing the k
+
+    # 
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+
+        res = r
+
+        while l < r:
+            k = l + (r - l) // 2
+            time = 0
+            
+            for pile in piles:
+                time += math.ceil(float(pile) / k)
+            
+            if time > h:
+                l = k + 1
+            else:
+                r = res = k
+        
+        return res
+        
+            
