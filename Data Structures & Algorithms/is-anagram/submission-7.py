2cb9ac2aba069cb3a3e70f67e9cd99ea12d82864
Serhii Kuzmin <sergeykuzmin495@gmail.com>
2026-08-16 17:38:29 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    # This is a warm up
+    # Total time: 3:26
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        freq = [0]*26
+
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
+        
+        for num in freq:
+            if num != 0:
+                return False
+        
+        return True 
+
+
+        
