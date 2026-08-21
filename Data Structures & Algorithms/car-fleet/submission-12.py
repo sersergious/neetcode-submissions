dd4509dd83d5908840d25a4aebf6cbe1f2ac2fc6
Serhii Kuzmin <sergeykuzmin495@gmail.com>
2026-08-20 20:18:05 -0400

    Add: car-fleet - submission-12

---
+++ Data Structures & Algorithms/car-fleet/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = list(zip(position, speed))        
+        cars.sort(reverse=True)
+        stack = []
+
+        for car in cars:
+            time = (target - car[0]) / car[1]
+            stack.append(time)
+
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack) 
+
